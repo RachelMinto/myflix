4a80bfbe9e7149908b9a63c6154185b36f9b632f
RachelMinto <rachel.j.minto@gmail.com>
2017-02-05 11:13:32 -0500

    All tests green for integrated my_queue reorder tests.

--- spec/features/my_queue_spec.rb
+++ spec/features/my_queue_spec.rb
@@ -11,43 +11,51 @@ feature 'user interacts with the queue' do
     futurama = Fabricate(:video, title: "Futurama", category: comedy)
     south_park = Fabricate(:video, title: "South Park", category: comedy)
     sign_in
-    find("a[href='/videos/#{monk.id}']").click
-    page.should have_content(monk.title)
-
-    click_link "+ My Queue"
-    page.should have_content(monk.title)
+    add_video_to_queue(monk)
+    expect_video_to_be_in_queue(monk)
 
     visit video_path(monk)
-    page.should_not have_content '+ My Queue'
+    expect_link_not_to_be_seen '+ My Queue'
 
+    add_video_to_queue(south_park)
+    add_video_to_queue(futurama)    
+
+    set_video_position(south_park, 1)
+    set_video_position(futurama, 2)
+    set_video_position(monk, 3)
+
+    update_queue
+
+    expect_video_position(south_park, 1)
+    expect_video_position(futurama, 2)
+    expect_video_position(monk, 3)
+  end
+
+  def add_video_to_queue(video)
     visit home_path
-    find("a[href='/videos/#{south_park.id}']").click
+    find("a[href='/videos/#{video.id}']").click
     click_link "+ My Queue"
+  end
 
-    visit home_path
-    find("a[href='/videos/#{futurama.id}']").click
-    click_link "+ My Queue"
+  def expect_video_to_be_in_queue(video)
+    page.should have_content(video.title)
+  end
 
-    fill_in("video_#{monk.id}", with: 3)
-    fill_in("video_#{futurama.id}", with: 2)
-    fill_in("video_#{south_park.id}", with: 1)
+  def expect_link_not_to_be_seen(link)
+    page.should_not have_content link
+  end
 
+  def set_video_position(video, position)
+    within(:xpath, "//tr[contains(.,'#{video.title}')]") do
+      fill_in "queued_videos[][position]", with: position
+    end
+  end
+
+  def expect_video_position(video, position)
+    expect(find(:xpath, "//tr[contains(.,'#{video.title}')]//input[@type='text']").value).to eq(position.to_s)  
+  end
+
+  def update_queue
     click_button "Update Instant Queue"
-
-    expect(find("#video_#{monk.id}").value).to eq("3")
-    expect(find("#video_#{futurama.id}").value).to eq("2")        
-    expect(find("#video_#{south_park.id}").value).to eq("1")    
   end
 end
-
-
-# - [x] Login
-# - [x] go to video
-# - [x] click add to my queue
-# - [x] visit my queue page
-# - [x] check that video is present
-# - [x] follow link to that video
-# - [ ] make sure it is same video check that + my_queue is not visible
-# - [ ] go to homepage and add more videos to queue
-# - [ ] go to my_queue and reorder videos in queue
-# - [ ] verify they come back in correct order
