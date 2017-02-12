1d47eb45aa3bb9b83acadc8dd9d31a72a7141833
RachelMinto <rachel.j.minto@gmail.com>
2017-02-12 17:10:22 -0500

    Changed from single to double quotes.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,11 +21,11 @@ Myflix::Application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
+    address:              "smtp.gmail.com,
     port:                 587,
-    domain:               'gmail.com',
-    user_name:            ENV['GMAIL_USERNAME'],
-    password:             ENV['GMAIL_PASSWORD'],
+    domain:               "gmail.com",
+    user_name:            ENV["GMAIL_USERNAME"],
+    password:             ENV["GMAIL_PASSWORD"],
     authentication:       'plain',
     enable_starttls_auto: true  }  
 end
