e667ecd5b3228df1acedb3ad3c02e8fd62681e2e
RachelMinto <rachel.j.minto@gmail.com>
2017-02-12 17:14:37 -0500

    Added line to config production to see error msg.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,6 +18,7 @@ Myflix::Application.configure do
 
   config.active_support.deprecation = :notify
 
+  config.consider_all_requests_local = true
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
