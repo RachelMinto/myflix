30fa78801d165e06bcba220c7207d02e6e1bcb1a
RachelMinto <rachel.j.minto@gmail.com>
2017-02-15 20:23:33 -0500

    Mailgun second deployment.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,26 +19,26 @@ Myflix::Application.configure do
   config.active_support.deprecation = :notify
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'localhost:3000'}  
-  config.action_mailer.smtp_settings = {
-    address:              "smtp.gmail.com",
-    port:                 587,
-    domain:               "gmail.com",
-    user_name:            ENV["GMAIL_USERNAME"],
-    password:             ENV["GMAIL_PASSWORD"],
-    authentication:       'plain',
-    enable_starttls_auto: true  }  
-end  
+#   config.action_mailer.default_url_options = { host: 'localhost:3000'}  
+#   config.action_mailer.smtp_settings = {
+#     address:              "smtp.gmail.com",
+#     port:                 587,
+#     domain:               "gmail.com",
+#     user_name:            ENV["GMAIL_USERNAME"],
+#     password:             ENV["GMAIL_PASSWORD"],
+#     authentication:       'plain',
+#     enable_starttls_auto: true  }  
+# end  
   
   # config.action_mailer.default_url_options = { host: 'localhost:3000'}
 
-#   ActionMailer::Base.smtp_settings = {
-#     :port           => ENV['MAILGUN_SMTP_PORT'],
-#     :address        => ENV['MAILGUN_SMTP_SERVER'],
-#     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-#     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-#     :domain         => ENV['domain'],
-#     :authentication => :plain,
-#   }
-#   ActionMailer::Base.delivery_method = :smtp
-# end
+  ActionMailer::Base.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => ENV['MAILGUN_DOMAIN'],
+    :authentication => :plain,
+  }
+  ActionMailer::Base.delivery_method = :smtp
+end
