3533d92cd177cd439a09aee1e3aa65309357756d
RachelMinto <rachel.j.minto@gmail.com>
2017-02-17 18:46:18 -0500

    Changed invite friend email to use background worker.

--- app/mailers/app_mailer.rb
+++ app/mailers/app_mailer.rb
@@ -4,13 +4,13 @@ class AppMailer < ActionMailer::Base
     mail to: user.email, from: "info@myflix.com", subject: "Welcome to Myflix!"
   end
 
+  def send_invitation_email(invite_id)
+    @invitation = Invitation.find(invite_id)   
+    mail to: @invitation.recipient_email, from: "info@myflix.com", subject: "Invitation to join Myflix"        
+  end
+
   def send_forgot_password(user)
     @user = user
     mail to: user.email, from: "info@myflix.com", subject: "Please reset your password."
-  end
-
-  def send_invitation_email(invitation)
-    @invitation = invitation
-    mail to: invitation.recipient_email, from: "info@myflix.com", subject: "Invitation to join Myflix"
   end
 end
