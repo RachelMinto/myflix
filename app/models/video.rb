2c8a344acfa4562afea381adc1a5f3ab7716aa1c
RachelMinto <rachel.j.minto@gmail.com>
2017-02-20 10:51:19 -0500

    Able to add images with new videos as an admin.

--- app/models/video.rb
+++ app/models/video.rb
@@ -3,8 +3,10 @@ class Video < ActiveRecord::Base
   has_many :reviews, -> { order "created_at DESC" }
   has_many :user_videos
   has_many :users, through: :user_videos
-  # validates_presence_of :title, :description, :small_cover_url, :large_cover_url
-  validates_presence_of :title, :description
+  validates_presence_of :title, :description, :small_cover, :large_cover
+
+  mount_uploader :large_cover, LargeCoverUploader
+  mount_uploader :small_cover, SmallCoverUploader  
 
   def self.search_by_title(search_term)
     return [] if search_term.blank?
